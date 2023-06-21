d74d890e3431d3321d437bb3b7d72958c563ccdc
CodeAGenie <nwibeg@gmail.com>
2023-06-21 02:51:35 +0100

    update

--- app.js
+++ app.js
@@ -4,29 +4,50 @@
 // The function should return result of numbers after applying the chosen operation.
 
 // SOLUTION
-function basicOp(operation, value1, value2){
-    switch (operation){
-        case '+':
-        return value1 + value2;
+// function basicOp(operation, value1, value2){
+//     switch (operation){
+//         case '+':
+//         return value1 + value2;
         
-        case '-':
-        return value1 - value2;
+//         case '-':
+//         return value1 - value2;
         
-        case '*':
-        return value1 * value2;
+//         case '*':
+//         return value1 * value2;
         
-        case '/':
-        return value1 / value2;
+//         case '/':
+//         return value1 / value2;
         
-        case '&&':
-        return value1 && value2;
+//         case '&&':
+//         return value1 && value2;
   
-    }
+//     }
     
-  }
+//   }
   
-  console.log(basicOp('+', 4, 5));
-  console.log(basicOp('-', 4, 5));
-  console.log(basicOp('*', 4, 5));
-  console.log(basicOp('/', 4, 5));
-  console.log(basicOp('&&',4, 5));
+//   console.log(basicOp('+', 4, 5));
+//   console.log(basicOp('-', 4, 5));
+//   console.log(basicOp('*', 4, 5));
+//   console.log(basicOp('/', 4, 5));
+//   console.log(basicOp('&&',4, 5));
+
+function basicOp(operation, value1, value2){
+        switch (operation){
+            case '+':
+            return value1 + value2;
+            
+            case '-':
+            return value1 - value2;
+            
+            case '*':
+            return value1 * value2;
+            
+            case '/':
+            return value1 / value2;
+            
+            case '&&':
+            return value1 && value2;
+      
+        }
+        
+      }
